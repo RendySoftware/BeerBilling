19a2fb950380b2ab3eb760a9a193ceb7fae63bed
vietthanh30 <vietthanh30@gmail.com>
2016-05-28 00:17:00 +0700

    add menu data

---
+++ doc/initialize_data.sql
@@ -0,0 +1,30 @@
+use BB
+go
+
+-- init category
+insert into Category(id, code, name) values(1, 'LAU', 'Thế giới lẩu')
+go
+
+insert into Category(id, code, name) values(2, 'THIT', 'Các món trâu tươi/Bò/Dê')
+go
+
+insert into Category(id, code, name) values(3, 'KHAIVI', 'Khai vị')
+go
+
+insert into Category(id, code, name) values(4, 'SALAT', 'Các món sa lát')
+go
+
+insert into Category(id, code, name) values(5, 'RAU', 'Rau xanh bốn mùa')
+go
+
+insert into Category(id, code, name) values(6, 'TUCHON', 'Món nhậu tự chọn')
+go
+
+insert into Category(id, code, name) values(7, 'COM', 'Các món cơm/canh/đồ mặn')
+go
+
+insert into Category(id, code, name) values(8, 'UONG', 'Đồ uống')
+go
+
+-- init menu
+ 
