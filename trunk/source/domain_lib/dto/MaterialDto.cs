5fd289e304f7442d6e35ca2116979deadf909897
vietthanh30 <vietthanh30@gmail.com>
2016-05-30 02:33:35 +0700

    add Dtos

---
+++ trunk/source/domain_lib/dto/MaterialDto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace domain_lib.dto
+{
+    public class MaterialDto
+    {
+        #region Declarations
+
+        // Property variables
+        private long _id = -1;
+
+        // Member variables
+        private long _unit_Id = -1;
+
+        private string _code = String.Empty;
+
+        private string _name = String.Empty;
+
+        private string _unitCode = String.Empty;
+
+        private string _unitName = String.Empty;
+
+        #endregion
+
+    	#region Constructor
+
+        public MaterialDto()
+        {
+        }
+
+    	#endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public virtual long Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        /// <summary>
+        /// UnitId.
+        /// </summary>
+        public virtual long UnitId
+        {
+            get { return _unit_Id; }
+            set { _unit_Id = value; }
+        }
+
+        /// <summary>
+        /// Code.
+        /// </summary>
+        public virtual string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public virtual string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        /// <summary>
+        /// UnitCode.
+        /// </summary>
+        public virtual string UnitCode
+        {
+            get { return _unitCode; }
+            set { _unitCode = value; }
+        }
+
+        /// <summary>
+        /// UnitName.
+        /// </summary>
+        public virtual string UnitName
+        {
+            get { return _unitName; }
+            set { _unitName = value; }
+        }
+
+        #endregion
+
+        #region Method Overrides
+
+        public override string ToString()
+        {
+            return _name;
+        }
+
+        #endregion
+
+    }
+}
---
+++ trunk/source/domain_lib/dto/UnitDto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace domain_lib.dto
+{
+    public class UnitDto
+    {
+        #region Declarations
+
+        // Property variables
+        private long _id = -1;
+
+        // Member variables
+        private string _code = String.Empty;
+
+        private string _name = String.Empty;
+
+        #endregion
+
+    	#region Constructor
+
+        public UnitDto()
+        {
+        }
+
+        public UnitDto(long id, string code, string name)
+        {
+            _id = id;
+            _code = code;
+            _name = name;
+        }
+
+    	#endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public virtual long Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        /// <summary>
+        /// Code.
+        /// </summary>
+        public virtual string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public virtual string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        #endregion
+
+        #region Method Overrides
+
+        public override string ToString()
+        {
+            return _name;
+        }
+
+        #endregion
+    }
+}
